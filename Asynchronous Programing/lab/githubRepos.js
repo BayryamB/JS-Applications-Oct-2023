fd2df5ac5d2eb6150900ac0ce88029e91813ed3f
Bayryam <bayryambashchoban@gmail.com>
2023-10-30 14:27:49 +0200

    using only JS files

---
+++ Asynchronous Programing/lab/githubRepos.js
@@ -0,0 +1,19 @@
+function loadRepos() {
+	const userName = document.getElementById('username');
+	const userRepos = document.getElementById('repos');
+	fetch(`https://api.github.com/users/${userName.value}/repos`)
+	.then(res => res.json())
+	.then(data => {
+		userRepos.innerHTML = '';
+		userName.value = '';
+
+		data.forEach(element => {
+			let li = document.createElement('li');
+			li.innerHTML = `<li><a href="${element.html_url}">${element.full_name}</a></li>`;
+			userRepos.appendChild(li);
+		});
+	})
+	.catch (err => console.log(err))
+	
+	
+}
