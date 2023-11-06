e77502d89a91be785906952ec2fbbffc2ac64e1b
Bayryam <bayryambashchoban@gmail.com>
2023-11-06 15:40:46 +0200

    modify app.js and loginpage

--- Remote Data and Authentication/EXERCISE/05.Fisher-Game/src/app.js
+++ Remote Data and Authentication/EXERCISE/05.Fisher-Game/src/app.js
@@ -1,1 +1,25 @@
-console.log('TODO:// Implement Home functionality');
+const loginBtnRef = document.getElementById('login');
+const registerBtnRef = document.getElementById('register');
+const logoutBtnRef = document.getElementById('logout');
+const homePageRef = document.getElementById('home');
+const welcomingMsgRef = document.querySelector('.email span');
+
+if(sessionStorage.getItem('userId')){
+    loginBtnRef.style.display = 'none';
+    registerBtnRef.style.display = 'none';
+    welcomingMsgRef.textContent = sessionStorage.getItem('email');
+}else{
+    welcomingMsgRef.textContent = `guest`
+    logoutBtnRef.style.display = 'none';
+}
+
+logoutBtnRef.addEventListener('click', async(e) => {
+    const response = await fetch(`http://localhost:3030/users/logout`, {
+        method: 'GET',
+        headers: {
+            "x-authorization": sessionStorage.getItem("accessToken")
+        }
+    })
+    sessionStorage.clear();
+    window.location.href = "index.html";
+});
--- Remote Data and Authentication/EXERCISE/05.Fisher-Game/src/login.js
+++ Remote Data and Authentication/EXERCISE/05.Fisher-Game/src/login.js
@@ -1,1 +1,32 @@
-console.log('TODO:// Implement Login functionality');
+document.querySelector('form').addEventListener('submit', onLoad);
+
+async function onLoad(e) {
+    e.preventDefault();
+    const form = new FormData(e.target);
+    const email = form.get('email');
+    const password = form.get('password');
+
+    if (!email ||!password) {
+        throw new Error('Invalid email or password');
+    }
+    const uriLogin = `http://localhost:3030/users/login`
+    const loginData = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "email": email,
+            "password": password.toString(),
+        })
+    }
+    try {
+        const response = await fetch(uriLogin, loginData);
+        const data = await response.json();
+        sessionStorage.setItem("email", data.email);
+        sessionStorage.setItem("accessToken", data.accessToken);
+        sessionStorage.setItem("userId", data._id);
+    } catch (error) {
+        throw new Error('Error fetching');
+    }
+}
