ea5379fc2a16890e56cbfe8c77cf2790c14428f8
Bayryam <bayryambashchoban@gmail.com>
2023-11-06 14:23:21 +0200

    logic for the register page

--- Remote Data and Authentication/EXERCISE/05.Fisher-Game/src/register.js
+++ Remote Data and Authentication/EXERCISE/05.Fisher-Game/src/register.js
@@ -1,1 +1,37 @@
-console.log('TODO:// Implement Register functionality');
+const formRef = document.getElementById('register-view');
+
+formRef.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const form = new FormData(e.target);
+    const email = form.get('email');
+    const password = form.get('password');
+    const repass = form.get('rePass');
+    
+    if(!email || !password || !repass || repass !== password) {
+        throw new Error ('Invalid email or password');
+    }
+    const uriReg = `http://localhost:3030/users/register`
+    const registerData = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "email": email,
+            "password" : password.toString(),
+        })
+    }
+    
+    try {
+        const response = await fetch(uriReg, registerData);
+        const data = await response.json();
+        console.log(data);
+        sessionStorage.setItem("email", data.email);
+        sessionStorage.setItem("accessToken", data.accessToken);
+        sessionStorage.setItem("userId", data._id);
+        window.location.href = "index.html";
+    } catch (error) {
+        throw new Error ('Problem fetching data');
+    }
+
+});
