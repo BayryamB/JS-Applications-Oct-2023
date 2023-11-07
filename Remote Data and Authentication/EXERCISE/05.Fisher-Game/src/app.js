95c3644886056a7650c30e3f8e5a604eae664a5f
Bayryam <bayryambashchoban@gmail.com>
2023-11-07 17:05:38 +0200

    add Catch is working

--- Remote Data and Authentication/EXERCISE/05.Fisher-Game/src/app.js
+++ Remote Data and Authentication/EXERCISE/05.Fisher-Game/src/app.js
@@ -28,55 +28,59 @@ logoutBtnRef.addEventListener('click', async(e) => {
     sessionStorage.clear();
     window.location.href = "index.html";
 });
-catchesFieldsetRef.innerHTML = '';
-loadBtnRef.addEventListener('click', async(e) => {
-    e.preventDefault();
-    const response = await fetch(`http://localhost:3030/data/catches`, {
-        method: 'GET',
-        headers: {
-            "Content-Type": "application/json",
-            //
-        }
-    });
-    const data = await response.json();
-    for (const obj in data) {
-        const angler = data[obj].angler;
-        const bait = data[obj].bait;
-        const captureTime = data[obj].captureTime;
-        const location = data[obj].location;
-        const weight = data[obj].weight;
-        const species = data[obj].species;
-        const ownerId = data[obj]._ownerId;
-        let div = document.createElement("div");
-        div.classList.add("catch");
-        div.innerHTML = `
-        <label>Angler</label>
-        <input type="text" class="angler" value="${angler}">
-        <label>Weight</label>
-        <input type="text" class="weight" value="${weight}">
-        <label>Species</label>
-        <input type="text" class="species" value="${species}">
-        <label>Location</label>
-        <input type="text" class="location" value="${location}">
-        <label>Bait</label>
-        <input type="text" class="bait" value="${bait}">
-        <label>Capture Time</label>
-        <input type="number" class="captureTime" value="${captureTime}">
-        <button class="update" data-id="${ownerId}" disabled>Update</button>
-        <button class="delete" data-id="${ownerId}" disabled>Delete</button>
-        `;
-        catchesFieldsetRef.appendChild(div);
-        if(ownerId === sessionStorage.getItem('userId')){
-            const btns = div.querySelectorAll('button');
-            let couplebtns = Array.from(btns);
-            couplebtns.forEach((btn) => {
-                btn.disabled = false;
-            });
+
+function solve() {
+    catchesFieldsetRef.innerHTML = '';
+    loadBtnRef.addEventListener('click', async(e) => {
+        e.preventDefault();
+        const response = await fetch(`http://localhost:3030/data/catches`, {
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/json",
+                //
+            }
+        });
+        const data = await response.json();
+        for (const obj in data) {
+            const angler = data[obj].angler;
+            const bait = data[obj].bait;
+            const captureTime = data[obj].captureTime;
+            const location = data[obj].location;
+            const weight = data[obj].weight;
+            const species = data[obj].species;
+            const ownerId = data[obj]._ownerId;
+            let div = document.createElement("div");
+            div.classList.add("catch");
+            div.innerHTML = `
+            <label>Angler</label>
+            <input type="text" class="angler" value="${angler}">
+            <label>Weight</label>
+            <input type="text" class="weight" value="${weight}">
+            <label>Species</label>
+            <input type="text" class="species" value="${species}">
+            <label>Location</label>
+            <input type="text" class="location" value="${location}">
+            <label>Bait</label>
+            <input type="text" class="bait" value="${bait}">
+            <label>Capture Time</label>
+            <input type="number" class="captureTime" value="${captureTime}">
+            <button class="update" data-id="${ownerId}" disabled>Update</button>
+            <button class="delete" data-id="${ownerId}" disabled>Delete</button>
+            `;
+            catchesFieldsetRef.appendChild(div);
+            if(ownerId === sessionStorage.getItem('userId')){
+                const btns = div.querySelectorAll('button');
+                let couplebtns = Array.from(btns);
+                couplebtns.forEach((btn) => {
+                    btn.disabled = false;
+                });
+            }
+            
         }
         
-    }
-    
-});
+    });
+}
+solve();
 
 addFormRef.addEventListener('submit', async (e) => {
     e.preventDefault();
@@ -89,6 +93,23 @@ addFormRef.addEventListener('submit', async (e) => {
     const species = form.get('species');
     const ownerId = sessionStorage.getItem('userId');
     const uriAdd = `http://localhost:3030/data/catches`;
-    //to do , add logic for add new catch /validating the info /
-    //
+    if(!angler || !captureTime || !species || !location || !weight || !bait){
+        throw new Error ('Fill the form with correct information');
+    }
+    const response = await fetch(uriAdd, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "x-authorization": sessionStorage.getItem("accessToken")
+        },
+        body: JSON.stringify({
+            "angler": angler,
+            "bait": bait,
+            "captureTime": captureTime,
+            "location": location,
+            "weight": weight,
+            "species": species,
+            "_ownerId": ownerId
+        })
+    });
 })
