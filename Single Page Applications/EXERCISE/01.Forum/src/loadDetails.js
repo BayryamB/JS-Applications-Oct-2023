d6c3c4f626422da1ffad3207ca00e9799d12fd3a
Bayryam <bayryambashchoban@gmail.com>
2023-11-09 16:27:01 +0200

    add comments field with working request

--- Single Page Applications/EXERCISE/01.Forum/src/loadDetails.js
+++ Single Page Applications/EXERCISE/01.Forum/src/loadDetails.js
@@ -1,6 +1,7 @@
 const commentsURI = `http://localhost:3030/jsonstore/collections/myboard/comments`;
 const main = document.querySelector('main');
 export function loadDetails(topicName, username, postText, date, postId){
+    
     const div = document.createElement('div');
     let postDate = new Date(date);
     let year =  postDate.getFullYear();
@@ -17,7 +18,91 @@ export function loadDetails(topicName, username, postText, date, postId){
         <p><span>${username}</span> posted on <time>${formattedDate}</time></p>
 
         <p class="post-content">${postText}</p>
+    </div>
+
+
+    <div id="user-comment">
+        <div class="topic-name-wrapper">
+            <div class="topic-name">
+                <p><strong>Daniel</strong> commented on <time>3/15/2021, 12:39:02 AM</time></p>
+                <div class="post-content">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure facere sint
+                        dolorem quam.</p>
+                </div>
+            </div>
+        </div>
     </div>`;
+    const userComment = document.createElement('div');
     main.innerHTML = ``;
     main.appendChild(div);
+    const currentUserCommentDiv = document.createElement('div');
+    currentUserCommentDiv.classList.add('answer-comment');
+    currentUserCommentDiv.innerHTML = `
+    <p><span>currentUser</span> comment:</p>
+    <div class="answer">
+        <form>
+            <textarea name="postText" id="comment" cols="30" rows="10"></textarea>
+            <div>
+                <label for="username">Username <span class="red">*</span></label>
+                <input type="text" name="username" id="username">
+            </div>
+            <button>Post</button>
+        </form>
+    </div>
+    ` ;
+    const postButton = currentUserCommentDiv.querySelector('button');
+    const commentInfo = currentUserCommentDiv.querySelector('#comment');
+    const usernameInput = currentUserCommentDiv.querySelector('#username');
+    postButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        addComments(commentInfo, usernameInput);
+        commentInfo.value = '';
+        usernameInput.value = '';
+        
+    });
+    main.appendChild(currentUserCommentDiv);
+
+    async function addComments(comment, username) {
+        let commentDate = new Date();
+        const response = await fetch(commentsURI, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "comment": comment.value,
+                "username": username.value,
+            })
+        })
+    }
 }
+/*<div class="answer-comment">
+                
+            </div>
+*/
+/*
+<div class="comment">
+    <div class="header">
+        <img src="./static/profile.png" alt="avatar">
+        <p><span>David</span> posted on <time>2020-10-10 12:08:28</time></p>
+
+        <p class="post-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure facere sint
+            dolorem quam,
+            accusantium ipsa veniam laudantium inventore aut, tenetur quibusdam doloribus. Incidunt odio
+            nostrum facilis ipsum dolorem deserunt illum?</p>
+    </div>
+
+
+    <div id="user-comment">
+        <div class="topic-name-wrapper">
+            <div class="topic-name">
+                <p><strong>Daniel</strong> commented on <time>3/15/2021, 12:39:02 AM</time></p>
+                <div class="post-content">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure facere sint
+                        dolorem quam.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+*/
