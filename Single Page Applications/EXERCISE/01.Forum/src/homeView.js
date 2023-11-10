4d9f432c9e1621bd5fba566acde4f72bae1cbec8
Bayryam <bayryambashchoban@gmail.com>
2023-11-10 17:37:38 +0200

    Everything is working , no bugs finded !

--- Single Page Applications/EXERCISE/01.Forum/src/homeView.js
+++ Single Page Applications/EXERCISE/01.Forum/src/homeView.js
@@ -1,63 +1,71 @@
-const topicContainer = document.querySelector('div.topic-container');
-const newPostDiv = document.querySelector('.new-topic-border');
+const main = document.querySelector('main');
 const postURI = `http://localhost:3030/jsonstore/collections/myboard/posts`;
+const div = document.createElement('div');
+
+div.classList.add('topic-container');
+
 import {loadDetails} from './loadDetails.js';
 //Loading the homepage
 export function loadHomePage() {
-    newPostDiv.innerHTML = `
-    <div class="header-background">
-        <span>New Topic</span>
-        </div>
-        <form>
-            <div class="new-topic-title">
-                <label for="topicName">Title <span class="red">*</span></label>
-                <input type="text" name="topicName" id="topicName">
-            </div>
-            <div class="new-topic-title">
-                <label for="username">Username <span class="red">*</span></label>
-                <input type="text" name="username" id="username">
-            </div>
-            <div class="new-topic-content">
-                <label for="postText">Post <span class="red">*</span></label>
-                <textarea type="text" name="postText" id="postText" rows="8" class="height"></textarea>
-            </div>
-            <div class="new-topic-buttons">
-                <button class="cancel">Cancel</button>
-                <button class="public">Post</button>
-            </div>
+    main.innerHTML = `
+    <div class="new-topic-border">
+                <div class="header-background">
+                    <span>New Topic</span>
+                </div>
+                <form>
+                    <div class="new-topic-title">
+                        <label for="topicName">Title <span class="red">*</span></label>
+                        <input type="text" name="topicName" id="topicName">
+                    </div>
+                    <div class="new-topic-title">
+                        <label for="username">Username <span class="red">*</span></label>
+                        <input type="text" name="username" id="username">
+                    </div>
+                    <div class="new-topic-content">
+                        <label for="postText">Post <span class="red">*</span></label>
+                        <textarea type="text" name="postText" id="postText" rows="8" class="height"></textarea>
+                    </div>
+                    <div class="new-topic-buttons">
+                        <button class="cancel">Cancel</button>
+                        <button class="public">Post</button>
+                    </div>
 
-        </form>`;
+                </form>
+            </div>
+    `;
+    main.appendChild(div);
 
     // Loading the topic form
     async function loadTopics(){
         try {
             const request = await fetch(postURI)
-        const data = await request.json();
+            const data = await request.json();
+
         for (const post in data) {
             let topicName = data[post].topicName;
             let username = data[post].username;
             let postText = data[post].postText;
             let date = data[post].date;
             let postId = data[post]._id;
-            const div = document.createElement('div');
-            div.classList.add('topic-name-wrapper');
-            div.innerHTML = `
-            <div class="topic-name">
-                <a href="#" class="normal">
-                    <h2>${topicName}</h2>
-                </a>
-                <div class="columns">
-                    <div>
+            const divWrapper = document.createElement('div');
+            divWrapper.classList.add('topic-name-wrapper');
+            divWrapper.innerHTML = `
+                <div class="topic-name">
+                    <a href="#" class="normal">
+                        <h2>${topicName}</h2>
+                    </a>
+                    <div class="columns">
+                        <div>
                         <p>Date: <time>${date}</time></p>
-                        <div class="nick-name">
-                            <p>Username: <span>${username}</span></p>
+                            <div class="nick-name">
+                                <p>Username: <span>${username}</span></p>
+                            </div>
                         </div>
+
+
                     </div>
-
-
-                </div>
-            </div>`
-            topicContainer.appendChild(div);
+                </div>`
+            div.appendChild(divWrapper);
             const titleAnchor = div.querySelector('.normal');
             titleAnchor.addEventListener('click', (e) => {
                 e.preventDefault();
@@ -80,7 +88,7 @@ export function loadHomePage() {
         });
         postButton.addEventListener('click', (e) => {
             e.preventDefault();
-            publishContent()
+            publishContent();
             clearInputs();
         });
     }
