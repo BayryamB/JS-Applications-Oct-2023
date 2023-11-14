8918c9d7acc37b4707b3a90a8655305121954315
Bayryam <bayryambashchoban@gmail.com>
2023-11-14 13:35:27 +0200

    add logic to switch button text and show info

--- Client Side Rendering/EXERCISE/02.HTTP-Status-Cats/app.js
+++ Client Side Rendering/EXERCISE/02.HTTP-Status-Cats/app.js
@@ -7,22 +7,18 @@ function catsLoader() {
         <ul>${cats.map(x => createLi(x))}</ul>
         `;
     renderer(templates, catsSection);
-    let buttons = document.querySelectorAll('button');
-    let btnArray = Array.from(buttons);
-    btnArray.forEach(btn => btn.addEventListener('click', (e) => {
-        e.preventDefault();
-        showHide(e.target);
-    }));
+    ;
 }
+catsLoader();
 function createLi(cat){
     return html`
     <li>
         <img src="./images/cat${cat.id}.jpg" width="250" height="250" alt="Card image cap">
         <div class="info">
-            <button class="showBtn" id="${cat.id}">Show status code</button>
+            <button class="showBtn" @click=${showHide}>Show status code</button>
                 <div class="status" style="display: none" id="${cat.id}">
                     <h4>${cat.statusCode}</h4>
-                    <p>Continue</p>
+                    <p>${cat.statusMessage}</p>
                 </div>
         </div>
     </li>`;
@@ -31,8 +27,9 @@ function renderer(code, ref) {
     render(code, ref)
 }
 function showHide(e) {
-    //to add logic for showing the status code!
-    
-
+    const button = e.target;
+    const div = button.parentElement.querySelector('div');
+    let currentState = div.style.display
+    div.style.display = currentState === 'none' ? 'block' : 'none';
+    button.textContent = currentState === 'none' ? 'Hide status code' : 'Show status code';
 }
-catsLoader();
