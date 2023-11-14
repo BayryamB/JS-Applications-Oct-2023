04f5acfea7656383fb8de77f50656aa6e291a925
Bayryam <bayryambashchoban@gmail.com>
2023-11-14 14:22:40 +0200

    add lit html to this project

--- Client Side Rendering/EXERCISE/03.Search-in-List/search.js
+++ Client Side Rendering/EXERCISE/03.Search-in-List/search.js
@@ -1,3 +1,22 @@
+import { towns} from './towns.js';
+import { html, render } from './node_modules/lit-html/lit-html.js';
 function search() {
-   // TODO
+   const townsDiv = document.getElementById('towns');
+   const searchBtn = document.querySelector('button');
+   const counter = document.querySelector('#result');
+
+   function createLi(x) {
+      return html`
+      <li>${x}</li>
+      `
+   }
+   function createContent () {
+      const content = html`
+      <ul>
+         ${towns.map(x => createLi(x))}
+      </ul>`;
+      render(content, townsDiv);
+   }
+   createContent();
 }
+search();
