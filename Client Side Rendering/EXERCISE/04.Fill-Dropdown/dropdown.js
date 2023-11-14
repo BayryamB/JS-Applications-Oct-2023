b80245f4e023ea02aae8e7f78c581ee8d196a3b9
Bayryam <bayryambashchoban@gmail.com>
2023-11-14 17:32:58 +0200

    add new elements in dropdown menu OK

--- Client Side Rendering/EXERCISE/04.Fill-Dropdown/dropdown.js
+++ Client Side Rendering/EXERCISE/04.Fill-Dropdown/dropdown.js
@@ -4,6 +4,10 @@ function addItem() {
     const selectRef = document.getElementById('menu');
     const inputText = document.querySelectorAll('input')[0];
     const addBtn = document.querySelectorAll('input')[1];
+    addBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        addNewOpttion(inputText.value);
+    })
     async function getItems(){
         const response = await fetch(url);
         const data = await response.json();
@@ -19,11 +23,26 @@ function addItem() {
     function createOption(obj) {
         let value = obj._id;
         let text = obj.text;
-        console.log(value);
         return html`
         <option value="${value}">${text}</option>
         `;
     }
     
+    async function addNewOpttion (value){
+        try {
+            const response = await fetch (url, {
+                method: 'POST',
+                headers: { 'Content-Type' : 'application/json'},
+                body: JSON.stringify ({
+                    "text": value
+                })
+            });
+            inputText.value = '';
+            getItems();
+        } catch (error) {
+            throw new Error (error.message)
+        }
+    }
+
 }
 addItem();
