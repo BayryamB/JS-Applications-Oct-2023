b5ec7df007e6a8493b3726fe33d962dd5d991d55
Bayryam <bayryambashchoban@gmail.com>
2023-11-14 17:14:05 +0200

    loading options is working

--- Client Side Rendering/EXERCISE/04.Fill-Dropdown/dropdown.js
+++ Client Side Rendering/EXERCISE/04.Fill-Dropdown/dropdown.js
@@ -1,3 +1,29 @@
+import { html, render } from "./node_modules/lit-html/lit-html.js";
 function addItem() {
-    console.log('TODO:...');
+    const url = 'http://localhost:3030/jsonstore/advanced/dropdown'
+    const selectRef = document.getElementById('menu');
+    const inputText = document.querySelectorAll('input')[0];
+    const addBtn = document.querySelectorAll('input')[1];
+    async function getItems(){
+        const response = await fetch(url);
+        const data = await response.json();
+        let arr = Object.values(data);
+        
+        const buff = html`
+        ${arr.map(obj => createOption(obj)
+        )}
+        `
+        render(buff, selectRef);
+    }
+    getItems();
+    function createOption(obj) {
+        let value = obj._id;
+        let text = obj.text;
+        console.log(value);
+        return html`
+        <option value="${value}">${text}</option>
+        `;
+    }
+    
 }
+addItem();
