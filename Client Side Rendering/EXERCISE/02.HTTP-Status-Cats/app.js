6f0d70a557510eaa731286c8a6b1f7fd8937555b
Bayryam <bayryambashchoban@gmail.com>
2023-11-14 14:04:17 +0200

    small modification  (now gives 100/100)

--- Client Side Rendering/EXERCISE/02.HTTP-Status-Cats/app.js
+++ Client Side Rendering/EXERCISE/02.HTTP-Status-Cats/app.js
@@ -6,26 +6,24 @@ function catsLoader() {
     let templates = html`
         <ul>${cats.map(x => createLi(x))}</ul>
         `;
-    renderer(templates, catsSection);
+    render(templates, catsSection);
     ;
 }
 catsLoader();
 function createLi(cat){
     return html`
     <li>
-        <img src="./images/cat${cat.id}.jpg" width="250" height="250" alt="Card image cap">
+        <img src="./images/${cat.imageLocation}.jpg" width="250" height="250" alt="Card image cap">
         <div class="info">
             <button class="showBtn" @click=${showHide}>Show status code</button>
-                <div class="status" style="display: none" id="${cat.id}">
-                    <h4>${cat.statusCode}</h4>
+                <div class="status" style="display: none">
+                    <h4>Status Code: ${cat.statusCode}</h4>
                     <p>${cat.statusMessage}</p>
                 </div>
         </div>
     </li>`;
 }
-function renderer(code, ref) {
-    render(code, ref)
-}
+
 function showHide(e) {
     const button = e.target;
     const div = button.parentElement.querySelector('div');
