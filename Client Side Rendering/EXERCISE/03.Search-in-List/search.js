605673e98015b392136c467f884b40568629c700
Bayryam <bayryambashchoban@gmail.com>
2023-11-14 14:48:15 +0200

    giving 100/100 in judge

--- Client Side Rendering/EXERCISE/03.Search-in-List/search.js
+++ Client Side Rendering/EXERCISE/03.Search-in-List/search.js
@@ -4,7 +4,7 @@ function search() {
    const townsDiv = document.getElementById('towns');
    const searchBtn = document.querySelector('button');
    const counter = document.querySelector('#result');
-
+   let count = 0;
    function createLi(x) {
       return html`
       <li>${x}</li>
@@ -18,5 +18,24 @@ function search() {
       render(content, townsDiv);
    }
    createContent();
+
+   searchBtn.addEventListener('click', (e) => {
+      e.preventDefault();
+      count = 0;
+      const searchedText = document.getElementById('searchText').value;
+      const items = Array.from(townsDiv.querySelectorAll('li'));
+      items.forEach((item) => matchCheck(item , searchedText));
+   });
+   function matchCheck(item, searchText) {
+      let townName = item.textContent;
+      if(townName.includes(searchText)){
+         item.classList.add('active');
+         count ++;
+      }else{
+         item.classList.remove('active');
+      }
+      counter.textContent = `${count} matches found`;
+   }
+   
 }
 search();
