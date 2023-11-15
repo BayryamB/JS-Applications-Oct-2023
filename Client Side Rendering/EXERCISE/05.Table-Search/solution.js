566f6b083906598dfd229b05a4f3cd2308e7ea56
Bayryam <bayryambashchoban@gmail.com>
2023-11-15 14:26:34 +0200

    add lit html and some basic logic

--- Client Side Rendering/EXERCISE/05.Table-Search/solution.js
+++ Client Side Rendering/EXERCISE/05.Table-Search/solution.js
@@ -1,8 +1,21 @@
+import { html, render } from "./node_modules/lit-html/lit-html.js";
+
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
+   const url = 'http://localhost:3030/jsonstore/advanced/table';
+   const inputRef = document.querySelector('#searchField');
+
+   async function loadData() {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+   }
+   loadData();
 
    function onClick() {
-      //   TODO:
+      inputValue = inputRef.value;
+
 
    }
 }
+solve();
