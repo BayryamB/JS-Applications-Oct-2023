a5ba58d93bab5376c5e7b2497c2521f1d016e8be
Bayryam <bayryambashchoban@gmail.com>
2023-11-15 15:12:10 +0200

    loading elements in table is working

--- Client Side Rendering/EXERCISE/05.Table-Search/solution.js
+++ Client Side Rendering/EXERCISE/05.Table-Search/solution.js
@@ -4,13 +4,35 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
    const url = 'http://localhost:3030/jsonstore/advanced/table';
    const inputRef = document.querySelector('#searchField');
+   const section = document.querySelector('tbody');
 
    async function loadData() {
       const response = await fetch(url);
       const data = await response.json();
-      console.log(data);
+      let arr = [];
+      for (const obj in data) {
+         arr.push(data[obj]);
+      }
+      const result = html`
+      ${arr.map(x => createContent(x))}
+      `;
+      render(result, section);
    }
    loadData();
+
+   function createContent(person) {
+      let firstName = person.firstName;
+      let lastName = person.lastName;
+      let email = person.email;
+      let course = person.course;
+      return html`
+      <tr>
+          <td>${firstName}${lastName}</td>
+          <td>${email}</td>
+          <td>${course}</td>
+      </tr>
+      `;
+   }
 
    function onClick() {
       inputValue = inputRef.value;
