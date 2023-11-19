892c422c8ceb29c5352354432e253892581b0ed2
Bayryam <bayryambashchoban@gmail.com>
2023-11-19 15:25:59 +0200

    delete project
    i will start from zero

--- Single Page Applications/EXERCISE/02.Movies/src/logedCheck.js
+++
@@ -1,7 +0,0 @@
-import { loadHome } from "./loadHome.js";
-export function logedCheck (){
-    if(localStorage.length > 0){
-        return true;
-    }
-    return false;
-}
--- Single Page Applications/EXERCISE/02.Movies/src/logout.js
+++
@@ -1,6 +0,0 @@
-import { loadHome } from "./loadHome.js";
-export function logout(){
-    console.log(`logout is working`);
-    localStorage.clear();
-    loadHome();
-}
