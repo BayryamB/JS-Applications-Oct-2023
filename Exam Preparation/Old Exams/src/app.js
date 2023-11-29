8f91be16191f2f2c30dbddad3412911ad084e941
Bayryam <bayryambashchoban@gmail.com>
2023-11-29 13:09:22 +0200

    login and logout are working

---
+++ Exam Preparation/Old Exams/src/app.js
@@ -0,0 +1,31 @@
+import page from "../node_modules/page/page.mjs";
+import { render } from "../node_modules/lit-html/lit-html.js";
+import { userService } from "./services/userService.js";
+import { homeView } from "./views/homeView.js";
+import { loginView } from "./views/loginView.js";
+import { registerPage } from "./views/registerView.js";
+import { logoutPage } from "./views/logout.js";
+import { create } from "./views/createView.js";
+
+page.start();
+updateNav();
+page.redirect('/');
+
+page('/', homeView);
+page('/login', loginView);
+page('/register', registerPage);
+page('/logout', logoutPage);
+page('/add', create);
+
+
+
+export function updateNav(){
+    const userData = userService.getUserData();
+    if(userData){
+        document.querySelector('.user').style.display = 'inline-block';
+        document.querySelector('.guest').style.display = 'none';
+    }else{
+        document.querySelector('.user').style.display = 'none';
+        document.querySelector('.guest').style.display = 'inline-block';
+    }
+}
